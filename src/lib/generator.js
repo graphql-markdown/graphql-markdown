d837a2352cbb04275071d2792bd9bb51bfc4598e
Gregory Heitz <edno@edno.io>
2020-06-27 18:28:37 +0200

    Prevent doc to generate if no schema change

--- src/lib/generator.js
+++ src/lib/generator.js
@@ -1,39 +1,40 @@
+const chalk = require('chalk');
 const { getSchemaMap, loadSchema, GraphQLFileLoader, UrlLoader, JsonFileLoader } = require('./graphql');
 const Renderer = require('./renderer');
 const Printer = require('./printer');
-const chalk = require('chalk');
-const { round } = require('lodash');
+const { checkSchemaChanges, saveSchemaHash, round } = require('./utils')
 
 const time = process.hrtime();
 
 module.exports = async function generateDocFromSchema(baseURL, schemaLocation, outputDir, homepageLocation) {
-    let r = undefined;
-    let pages = [];
-    let schema = undefined;
-    let rootTypes = [];
-    return Promise.resolve(
-        loadSchema(schemaLocation, {
-            loaders: [new GraphQLFileLoader(), new UrlLoader(), new JsonFileLoader()],
-        }),
-    )
-        .then((s) => (schema = s))
-        .then(() => (r = new Renderer(new Printer(schema, baseURL), outputDir, baseURL)))
-        .then(() => (rootTypes = getSchemaMap(schema)))
-        .then(() =>
-            Promise.all(Object.keys(rootTypes).map((typeName) => r.renderRootTypes(typeName, rootTypes[typeName]))),
-        )
+  return Promise.resolve(
+    loadSchema(schemaLocation, {
+      loaders: [new GraphQLFileLoader(), new UrlLoader(), new JsonFileLoader()],
+    }),
+  )
+  .then((schema) => { 
+    if (checkSchemaChanges(schema, outputDir)) {
+      let pages = [];
+      const r = new Renderer(new Printer(schema, baseURL), outputDir, baseURL);
+      const rootTypes = getSchemaMap(schema);
+      await Promise.all(Object.keys(rootTypes).map((typeName) => r.renderRootTypes(typeName, rootTypes[typeName])))
         .then((p) => {
-            pages = p.reduce((r, i) => [].concat(r, i), []);
+          pages = p.reduce((r, i) => [].concat(r, i), []);
         })
         .then(() => r.renderHomepage(homepageLocation))
         .then(() => r.renderSidebar(pages))
         .then((sidebarPath) => {
-            const [sec, msec] = process.hrtime(time);
-            const duration = round(sec + msec / 1000000000, 3);
-            console.info(
-                chalk.green(`Documentation succesfully generated in "${outputDir}" with base URL "${baseURL}"`),
-            );
-            console.log(chalk.blue(`${pages.length} pages generated in ${duration}s from schema "${schemaLocation}"`));
-            console.info(chalk.blue.bold(`Remember to update your Docusaurus site's sidebars with "${sidebarPath}"`));
+          const [sec, msec] = process.hrtime(time);
+          const duration = round(sec + msec / 1000000000, 3);
+          console.info(
+            chalk.green(`Documentation succesfully generated in "${outputDir}" with base URL "${baseURL}".`),
+          );
+          console.log(chalk.blue(`${pages.length} pages generated in ${duration}s from schema "${schemaLocation}".`));
+          console.info(chalk.blue.bold(`Remember to update your Docusaurus site's sidebars with "${sidebarPath}".`));
         });
+      saveSchemaHash(schema, outputDir); 
+    } else {
+      console.info(chalk.blue(`No changes detected in schema "${schemaLocation}".`));
+    }
+  })
 };
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,11 @@
 const slugify = require('slugify');
-const { kebabCase } = require('lodash');
+const { kebabCase, startCase, round } = require('lodash');
+const crypto = require('crypto');
+const { printSchema } = require('graphql')
+const fs = require('fs-extra')
+const path = require('path')
 
-const { startCase } = require('lodash');
+const SCHEMA_HASH_FILE ='.schema';
 
 function toSlug(str) {
     return slugify(kebabCase(str));
@@ -12,4 +16,28 @@ function toArray(param) {
     return undefined;
 }
 
-module.exports = { startCase, toSlug, toArray };
+function getSchemaHash(schema) {
+  let printedSchema = printSchema(schema, { commentDescriptions: true })
+  let sum = crypto.createHash('sha256');
+  sum.update(printedSchema);
+  return sum.digest('hex');
+}
+
+function checkSchemaChanges(schema, outputDir) {
+  const hashFile = path.join(outputDir, SCHEMA_HASH_FILE);
+  const hashSchema = getSchemaHash(schema);
+  let hasDiff = true;
+  if (fs.existsSync(hashFile)) {
+    const hash = fs.readFileSync(hashFile);
+    hasDiff = hashSchema != hash;
+  }
+  return hasDiff;
+}
+
+function saveSchemaHash(schema, outputDir) {
+  const hashFile = path.join(outputDir, SCHEMA_HASH_FILE);
+  const hashSchema = getSchemaHash(schema);
+  fs.outputFileSync(hashFile, hashSchema);
+}
+
+module.exports = { round, startCase, toSlug, toArray, getSchemaHash, checkSchemaChanges, saveSchemaHash };
