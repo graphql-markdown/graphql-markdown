e449357eed3152d167eaa3e34b2bd9e2bc744535
Gregory Heitz <edno@edno.io>
2020-10-09 16:47:17 +0200

    Use Jest projects

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  projects: ["<rootDir>/tests/*"],
+  verbose: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
+  collectCoverage: true,
+  coverageReporters: ["text"],
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).js"],
+  rootDir: __dirname,
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
