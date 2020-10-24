abacc4155b3bc32ae368b35e816128f451996a64
Gregory Heitz <edno@edno.io>
2020-10-24 03:30:45 +0200

    [WIP] Attempt to add code coverage

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  testEnvironment: "node",
-  verbose: true,
-  collectCoverage: false,
-  rootDir: __dirname,
   name: "e2e",
   displayName: "End-to-End Tests",
-  testMatch: [`${__dirname}/**/?(*.)+(spec|test).js`],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
+  testEnvironment: "node",
+  verbose: true,
+  rootDir: __dirname,
+  testMatch: ["<rootDir>/**/?(*.)+(spec|test).js"],
+  collectCoverageFrom: [
+    "/usr/src/app/docusaurus2-graphql-doc-generator/src/**/*.js",
+  ],
+  collectCoverage: true,
+  coverageReporters: ["json"],
+  coverageDirectory: "<rootDir>/.nyc_output/e2e",
 };
