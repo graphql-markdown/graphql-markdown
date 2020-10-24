f49a514a3ba7ef743ed1891c9b040bb563342edd
Gregory Heitz <edno@edno.io>
2020-10-24 21:38:44 +0200

    Improve docusaurus e2e config

--- tests/e2e/scripts/config-plugin.js
+++ tests/e2e/scripts/config-plugin.js
@@ -10,6 +10,24 @@ let config = require(docusaurusConfig);
 
 config = {
   ...config,
+  title: "GraphQL to Doc",
+  tagline: "Markdown documentation from a GraphQL schema.",
+  organizationName: "edno",
+  projectName: "docusaurus2-graphql-doc",
+  themeConfig: {
+    ...config.themeConfig,
+    navbar: {
+      ...config.themeConfig.navbar,
+      items: [
+        {
+          to: "docs/schema",
+          activeBasePath: "docs",
+          label: "Schema",
+          position: "left",
+        },
+      ],
+    },
+  },
   plugins: [["@edno/docusaurus2-graphql-doc-generator", "@config@"]],
 };
 
@@ -24,5 +42,19 @@ fs.writeFile(docusaurusConfig, configExportString, (err) => {
     console.log(`Error updating '${docusaurusConfig}'`, err);
   } else {
     console.log(`Successfully updated '${docusaurusConfig}'`);
+  }
+});
+
+const sidebarConfig = require.resolve(`${process.cwd()}/sidebars.js`);
+
+const sidebarConfigString = `const path = require("path");
+const pluginConfig = require(path.resolve(__dirname, "${pluginConfigFilename}"));
+module.exports = require(path.resolve(__dirname, pluginConfig.rootPath, "sidebar-schema.js"));\n`;
+
+fs.writeFile(sidebarConfig, sidebarConfigString, (err) => {
+  if (err) {
+    console.log(`Error updating '${sidebarConfig}'`, err);
+  } else {
+    console.log(`Successfully updated '${sidebarConfig}'`);
   }
 });
