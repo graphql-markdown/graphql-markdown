c71968c1ebc9da69cdada5f4fd6009894938a3d0
Gregory Heitz <edno@edno.io>
2020-10-24 15:31:31 +0200

    Remove code coverage

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -5,10 +5,5 @@ module.exports = {
   verbose: true,
   rootDir: __dirname,
   testMatch: ["<rootDir>/**/?(*.)+(spec|test).js"],
-  collectCoverageFrom: [
-    "/usr/src/app/docusaurus2-graphql-doc-generator/src/**/*.js",
-  ],
-  collectCoverage: true,
-  coverageReporters: ["json"],
-  coverageDirectory: "<rootDir>/.nyc_output/e2e",
+  collectCoverage: false,
 };
