5782927543b2d359133f02fcf69853f5bd37bfc3
Gregory Heitz <edno@edno.io>
2021-05-15 15:08:49 +0200

    remove files

--- packages/docusaurus2-graphql-doc-generator/tests/integration/jest.config.js
+++
@@ -1,9 +0,0 @@
-const jestConfigBase = require("../../jest.config");
-
-module.exports = {
-  ...jestConfigBase,
-  name: "integration",
-  displayName: "Integration Tests",
-  testMatch: [`${__dirname}/**/?(*.)+(spec|test).js`],
-  coverageDirectory: `${jestConfigBase.coverageDirectory}/integration`,
-};
--- packages/docusaurus2-graphql-doc-generator/tests/unit/jest.config.js
+++
@@ -1,9 +0,0 @@
-const jestConfigBase = require("../../jest.config");
-
-module.exports = {
-  ...jestConfigBase,
-  name: "unit",
-  displayName: "Unit Tests",
-  testMatch: [`${__dirname}/**/?(*.)+(spec|test).js`],
-  coverageDirectory: `${jestConfigBase.coverageDirectory}/unit`,
-};
