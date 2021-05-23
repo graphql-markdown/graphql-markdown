63254222ba299662d62c26e552000a42964f8852
Gregory Heitz <edno@edno.io>
2021-05-23 14:24:32 +0200

    fix lint issues

--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -1,23 +1,23 @@
 import { parseSchema } from "../src/lib/parser";
 
-import { promises as fs } from "fs";
 import * as path from "path";
+import { promises as fs } from "fs";
 
 import { DocumentNode } from "graphql";
 
 describe("parseSchema", () => {
-  let schema: DocumentNode;
+  let schema: DocumentNode; // eslint-disable-line init-declarations
 
   beforeAll(async () => {
     const schemaLocation = path.resolve(
       `${__dirname}/__data__/schema/tweet.json`
     );
-    schema = JSON.parse((
-      await fs.readFile(schemaLocation)
-    ).toString()) as unknown as DocumentNode;
+    schema = JSON.parse(
+      (await fs.readFile(schemaLocation)).toString()
+    ) as unknown as DocumentNode;
   });
 
-  it("should returned a parsed schema ", () => {
+  it("should returned a parsed schema", () => {
     const parsed = parseSchema(schema);
     expect(parsed).toMatchSnapshot();
   });
