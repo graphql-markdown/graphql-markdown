06bcae9c7e0eecbe27221d503612e0aafb3135de
Gregory Heitz <edno@edno.io>
2021-06-02 20:55:35 +0200

    update tests

--- tests/e2e/scripts/start-demo.sh
+++ tests/e2e/scripts/start-demo.sh
@@ -1,5 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
+
+cd /usr/src/app/docusaurus2
 
 npx docusaurus graphql-to-doc
 
-yarn start --host=0.0.0.0 --port=8080
+yarn build --no-minify
+
+yarn serve --host=0.0.0.0 --port=8080
