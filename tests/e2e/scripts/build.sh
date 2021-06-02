f1ca5bc4139c35dc8902abb48230c550153e039c
Gregory Heitz <edno@edno.io>
2021-06-02 22:36:27 +0200

    fix build & start e2e demo

--- tests/e2e/scripts/build.sh
+++ tests/e2e/scripts/build.sh
@@ -7,5 +7,3 @@ cd /usr/src/app/docusaurus2
 npx docusaurus graphql-to-doc
 
 yarn build --no-minify
-
-yarn serve --host=0.0.0.0 --port=8080
---
+++ tests/e2e/scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ./build.sh
+
+yarn serve --host=0.0.0.0 --port=8080
