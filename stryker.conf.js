7ddb755ea5e487d7eadc5a3a8b0f8c58e81c2b5e
Gregory Heitz <edno@users.noreply.github.com>
2021-09-22 10:37:56 +0200

    fix striker jest config

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,10 +4,16 @@ module.exports = {
   thresholds: { high: 80, low: 60, break: 50 },
   testRunner: "jest",
   coverageAnalysis: "perTest",
-  ignorePatterns: ["!node_modules", "tests/e2e/**"],
-  symlinkNodeModules: false,
+  mutate: ["src/**/*.js", "!src/**/index.js"],
   jest: {
     projectType: "custom",
     configFile: "jest.config.js",
+    config: {
+      bail: false,
+      collectCoverage: false,
+      notify: false,
+      reporters: [],
+      verbose: false,
+    },
   },
 };
