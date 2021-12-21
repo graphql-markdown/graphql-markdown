b4025301e2bc5acb1a494f721152a9a600396000
Gregory Heitz <324670+edno@users.noreply.github.com>
2021-12-21 12:36:59 +0100

    fix smoke test failing on node warning

--- tests/helpers/cli/index.js
+++ tests/helpers/cli/index.js
@@ -4,7 +4,7 @@ module.exports = function cli(args = [], cwd = "/docusaurus2") {
   return new Promise((resolve) => {
     exec(
       `npx docusaurus graphql-to-doc ${args.join(" ")}`,
-      { cwd },
+      { cwd, env: { ...process.env, NODE_NO_WARNINGS: 1 } },
       (error, stdout, stderr) => {
         resolve({
           code: error && error.code ? error.code : 0,
