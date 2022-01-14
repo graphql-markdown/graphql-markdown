1913e616f98ed15bfc4f97f5e291c8f2d5ed8b29
Grégory Heitz <324670+edno@users.noreply.github.com>
2022-01-14 16:46:29 +0100

    remove picocolors dependency (#422)
    * remove picocolors dependency
    
    * remove last pico
    
    * fix dangerfile
    
    Co-authored-by: Gregory Heitz <edno@users.noreply.github.com>

--- dangerfile.js
+++ dangerfile.js
@@ -1,15 +1,6 @@
 const { danger, fail, warn, schedule } = require("danger");
 const Diff = require("diff");
 const filter = require("lodash.filter");
-const pico = require("picocolors");
-
-const COLOR = {
-  ADDED: pico.green,
-  REMOVED: pico.red,
-  COMMON: pico.gray,
-  FAIL: pico.red,
-  WARN: pico.yellow,
-};
 
 const PACKAGE_LOCK = "package-lock.json";
 const PACKAGE_JSON = "package.json";
@@ -28,30 +19,21 @@ const dangerFile = danger.git.fileMatch(DANGER_FILE);
 
 const getDiffDependencies = (dependencies) => {
   const diff = Diff.diffJson(dependencies.before, dependencies.after);
-  let colorDiff = ``;
+  let stringDiff = ``;
   diff.forEach((part) => {
-    const color = part.added
-      ? COLOR.ADDED
-      : part.removed
-      ? COLOR.REMOVED
-      : COLOR.COMMON;
-    colorDiff = `${colorDiff} ${color(part.value)}`;
+    stringDiff = `${stringDiff} ${part.value}`;
   });
-  return colorDiff;
+  return stringDiff;
 };
 
 // rule-package-lock-detected
 if (packageLock.modified || packageLock.created) {
-  fail(
-    COLOR.FAIL(
-      `\`${PACKAGE_LOCK}\` detected, you must used 'yarn' for dependencies.`,
-    ),
-  );
+  fail(`\`${PACKAGE_LOCK}\` detected, you must used 'yarn' for dependencies.`);
 }
 
 // rule-yarn-lock-deleted
 if (yarnLock.deleted) {
-  fail(COLOR.FAIL(`This PR deleted the \`${YARN_LOCK}\` file.`));
+  fail(`This PR deleted the \`${YARN_LOCK}\` file.`);
 }
 
 // rule-yarn-lock-not-updated
@@ -59,15 +41,13 @@ if (packageJson.modified && !(yarnLock.modified || yarnLock.created)) {
   schedule(async () => {
     const packageDiff = await danger.git.JSONDiffForFile(PACKAGE_JSON);
     if (packageDiff.dependencies) {
-      const description = COLOR.FAIL(
-        "Dependencies changed with no corresponding lockfile changes:",
-      );
+      const description =
+        "Dependencies changed with no corresponding lockfile changes:";
       fail(`${description}\n${getDiffDependencies(packageDiff.dependencies)}`);
     }
     if (packageDiff.devDependencies) {
-      const description = COLOR.FAIL(
-        "Dev dependencies changed with no corresponding lockfile changes:",
-      );
+      const description =
+        "Dev dependencies changed with no corresponding lockfile changes:";
       fail(
         `${description}\n${getDiffDependencies(packageDiff.devDependencies)}`,
       );
@@ -77,17 +57,17 @@ if (packageJson.modified && !(yarnLock.modified || yarnLock.created)) {
 
 // rule-license-file-modified
 if (licenseFile.modified) {
-  warn(COLOR.WARN(`This PR modified the \`${LICENSE_FILE}\` file.`));
+  warn(`This PR modified the \`${LICENSE_FILE}\` file.`);
 }
 
 // rule-license-file-deleted
 if (licenseFile.deleted) {
-  fail(COLOR.FAIL(`This PR deleted the \`${LICENSE_FILE}\` file.`));
+  fail(`This PR deleted the \`${LICENSE_FILE}\` file.`);
 }
 
 // rule-readme-file-deleted
 if (readmeFile.deleted) {
-  fail(COLOR.FAIL(`This PR deleted the \`${README_FILE}\` file.`));
+  fail(`This PR deleted the \`${README_FILE}\` file.`);
 }
 
 const jestSnapshots = {
@@ -101,17 +81,17 @@ const jestSnapshots = {
 
 // rule-jest-snapshot-modified
 if (jestSnapshots.modified.length > 0) {
-  const description = COLOR.WARN("This PR modified some Jest snapshot file/s:");
+  const description = "This PR modified some Jest snapshot file/s:";
   warn(`${description}\n - ${jestSnapshots.modified.join("\n - ")}`);
 }
 
 // rule-jest-snapshot-deleted
 if (jestSnapshots.deleted.length > 0) {
-  const description = COLOR.WARN("This PR deleted some Jest snapshot file/s:");
+  const description = "This PR deleted some Jest snapshot file/s:";
   warn(`${description}\n - ${jestSnapshots.deleted.join("\n - ")}`);
 }
 
 // rule-danger-file-modified
 if (dangerFile.modified) {
-  warn(COLOR.WARN(`This PR modified the \`${DANGER_FILE}\` file.`));
+  warn(`This PR modified the \`${DANGER_FILE}\` file.`);
 }
