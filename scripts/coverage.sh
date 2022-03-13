0f1a58827246a0eac864e7a5efbc8a69260f02ed
Gregory Heitz <edno@users.noreply.github.com>
2022-03-13 17:44:22 +0100

    coverage script formatting

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,7 +5,7 @@ COVERAGE_DIR=.nyc_output
 COVERAGE_FILE=coverage-final.json
 
 for FILE in $(find $COVERAGE_DIR -name $COVERAGE_FILE); do
-  mv "$FILE" $COVERAGE_DIR/$(basename $(dirname $FILE)).json
+  mv "$FILE" $COVERAGE_DIR/$(basename $(dirname "$FILE")).json
 done
 npx -y --quiet nyc merge $COVERAGE_DIR $COVERAGE_DIR/coverage.json
-npx -y --quiet nyc report --reporter=lcov  --reporter=text --check-coverage
+npx -y --quiet nyc report --reporter=lcov --reporter=text --check-coverage
