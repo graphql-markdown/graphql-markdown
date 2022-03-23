122cb5f5e8f328d2f78b280fab91234e3b2a94b7
Gregory Heitz <edno@users.noreply.github.com>
2022-03-23 22:53:49 +0100

    :wrench:  update stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,8 +1,4 @@
 module.exports = {
-  commandRunner: {
-    command:
-      "NODE_ENV=ci node --expose-gc ./node_modules/.bin/jest --logHeapUsage --runInBand",
-  },
   coverageAnalysis: "perTest",
   jest: {
     config: {
@@ -19,5 +15,6 @@ module.exports = {
   packageManager: "yarn",
   reporters: ["clear-text", "progress", "html"],
   testRunner: "jest",
+  testRunnerNodeArgs: ["--experimental-vm-modules"],
   thresholds: { high: 80, low: 60, break: 50 },
 };
