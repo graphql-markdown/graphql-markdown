33cf9499427cd02124d6a11615ddd5e552f9ebe3
Grégory Heitz <edno@edno.io>
2022-05-27 09:23:21 +0200

    :recycle:  clean up helpers lib format

--- src/utils/scalars/array.js
+++ src/utils/scalars/array.js
@@ -1,6 +1,7 @@
 /**
  * Array functions
  */
+
 function toArray(param) {
   if (param && typeof param === "object")
     return Object.keys(param).map((key) => param[key]);
--- src/utils/scalars/object.js
+++ src/utils/scalars/object.js
@@ -1,12 +1,14 @@
 /**
  * Object functions
  */
+
 function hasProperty(obj, prop) {
   return (
     !!(obj && obj[prop]) ||
     (obj instanceof Object && Object.prototype.hasOwnProperty.call(obj, prop))
   );
 }
+
 function hasMethod(obj, prop) {
   return hasProperty(obj, prop) && typeof obj[prop] === "function";
 }
