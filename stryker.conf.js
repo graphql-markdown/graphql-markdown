8b27533988820dcf466e1a9b8bb1b42e77ca67fe
Grégory Heitz <edno@edno.io>
2022-07-16 21:20:36 +0200

    :wrench:  update stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,4 +1,6 @@
 module.exports = {
+  inPlace: true,
+  logLevel: "error",
   coverageAnalysis: "perTest",
   jest: {
     config: {
@@ -10,11 +12,12 @@ module.exports = {
     },
     configFile: "jest.config.js",
     projectType: "custom",
+    enableFindRelatedTests: true,
   },
   mutate: ["src/**/*.js", "!src/**/index.js", "!src/**/prettier.js"],
   packageManager: "yarn",
   reporters: ["clear-text", "progress", "html"],
   testRunner: "jest",
   testRunnerNodeArgs: ["--experimental-vm-modules"],
-  thresholds: { high: 80, low: 60, break: 50 },
+  thresholds: { high: 85, low: 75, break: 70 },
 };
