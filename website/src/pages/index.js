f9110eafa2408cd570e75a4aeaedabceef795a30
Grégory Heitz <edno@edno.io>
2022-09-24 21:19:35 +0200

    fix sonarcloud issues

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,7 +8,6 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--secondary', styles.heroBanner)}>
       <div className="container">
