93fa49861c83f0482513320fb481d4b28d132221
Grégory Heitz <edno@edno.io>
2022-10-14 05:10:01 +0000

    update jest config

--- tools/jest.config.js
+++ tools/jest.config.js
@@ -1,16 +1,13 @@
-const path = require("path");
-
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/packages/**/src/**/*.js"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
   coverageDirectory: "<rootDir>/.nyc_output",
   coverageReporters: ["json"],
   globals: {
     __OS__: require("os").platform() === "win32" ? "windows" : "unix",
   },
-  projects: ["<rootDir>/packages/docusaurus/tests/unit", "<rootDir>/packages/docusaurus/tests/integration"],
-  rootDir: path.join(__dirname, ".."),
-  setupFilesAfterEnv: ["<rootDir>/packages/docusaurus/tests/jest.setup.js"],
+  projects: ["<rootDir>/tests/unit", "<rootDir>/tests/integration"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "node",
   testMatch: ["**/tests/(unit|integration)/**/*.(test|spec).js"],
   watchPathIgnorePatterns: ["__expected__"],
