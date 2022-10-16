55d55a4eb93fd28d123cd3a3001ecd338fb51bcb
Grégory Heitz <edno@edno.io>
2022-10-16 19:01:29 +0000

    fix test coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-COVERAGE_DIR=.nyc_output
+COVERAGE_DIR=coverage
 COVERAGE_FILE=coverage-final.json
 
 for FILE in $(find $COVERAGE_DIR -name $COVERAGE_FILE); do
   mv "$FILE" $COVERAGE_DIR/$(basename $(dirname "$FILE")).json
 done
 npx -y --quiet nyc merge $COVERAGE_DIR $COVERAGE_DIR/coverage.json
-npx -y --quiet nyc report --reporter=lcov --reporter=text --check-coverage
+npx -y --quiet nyc report --reporter=lcov --reporter=text --check-coverage --temp-dir coverage
