6b3a5a5bbc858a92143da52c0fa4826aed70543f
Grégory Heitz <edno@edno.io>
2022-10-23 08:02:12 +0200

    :construction_worker:  update stryker configs

--- packages/docusaurus/stryker.conf.js
+++ packages/docusaurus/stryker.conf.js
@@ -1,1 +1,5 @@
-module.exports = require("@graphql-markdown/tools-config").stryker;
+const config = require("@graphql-markdown/tools-config").stryker;
+
+config.dashboard.module = "docusaurus";
+
+module.exports = config;
--- packages/utils/stryker.conf.js
+++ packages/utils/stryker.conf.js
@@ -1,1 +1,5 @@
-module.exports = require("@graphql-markdown/tools-config").stryker;
+const config = require("@graphql-markdown/tools-config").stryker;
+
+config.dashboard.module = "utils";
+
+module.exports = config;
