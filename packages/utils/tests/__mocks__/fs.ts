bb4655af66e69ad7240f279294f0f42da40725d3
Grégory Heitz <edno@edno.io>
2022-11-12 18:00:36 +0000

    more migration from jest to tap

--- packages/utils/tests/__mocks__/fs.ts
+++
@@ -1,6 +0,0 @@
-import { vol } from "memfs";
-import { ufs } from "unionfs";
-
-const fs = jest.requireActual("fs");
-
-export default ufs.use(fs).use(vol as any);
--- packages/utils/tests/unit/fs.test.ts
+++ packages/utils/tests/unit/fs.test.ts
@@ -1,81 +1,82 @@
-import { vol } from "memfs";
-jest.mock("fs");
+import t from "tap";
 
 import { ensureDir, fileExists, saveFile } from "../../src/fs";
 
-describe("fs", () => {
-  beforeEach(() => {
-    vol.fromJSON({
-      "/testFolder": "",
-      "/testFolder/testFile": "just a test",
-    });
-  });
+import { vol } from "memfs";
 
-  afterEach(() => {
-    vol.reset();
-  });
-
-  describe("fileExists()", () => {
-    test.each([
-      {
-        type: "file",
-        path: "/testFolder/testFile",
-        expected: true,
-        desc: "exists",
-      },
-      { type: "folder", path: "/testFolder", expected: true, desc: "exists" },
-      {
-        type: "file",
-        path: "/testFolder/missingFile",
-        expected: false,
-        desc: "not exists",
-      },
-      {
-        type: "folder",
-        path: "/missingFolder",
-        expected: false,
-        desc: "not exists",
-      },
-    ])(
-      "return $expected if $type '$path' $desc",
-      async ({ path, expected }) => {
-        expect.assertions(1);
-
-        await expect(fileExists(path)).resolves.toBe(expected);
-      }
-    );
-  });
-
-  describe("ensureDir()", () => {
-    test.each([
-      { path: "/testFolder", exists: true, desc: "not created if present" },
-      {
-        path: "/testNewFolder",
-        exists: false,
-        desc: "created if not present",
-      },
-    ])("folder is", async ({ path, exists }) => {
-      expect.assertions(2);
-
-      await expect(fileExists(path)).resolves.toBe(exists);
-
-      await ensureDir(path);
-
-      await expect(fileExists(path)).resolves.toBeTruthy();
-    });
+t.beforeEach(() => {
+  vol.fromJSON({
+    "/testFolder": "",
+    "/testFolder/testFile": "just a t.test",
   });
 });
 
-describe("saveFile()", () => {
-  test("create file and folders", async () => {
-    expect.assertions(1);
+t.afterEach(() => {
+  vol.reset();
+});
 
-    await saveFile("/foo/bar/test/foobar.test", "foobar file for test");
+t.test("fileExists()", async () => {
+  const data: {type: string, path:string, expected: boolean, desc: string}[] = [
+    {
+      type: "file",
+      path: "/testFolder/testFile",
+      expected: true,
+      desc: "exists",
+    },
+    { type: "folder", path: "/testFolder", expected: true, desc: "exists" },
+    {
+      type: "file",
+      path: "/testFolder/missingFile",
+      expected: false,
+      desc: "not exists",
+    },
+    {
+      type: "folder",
+      path: "/missingFolder",
+      expected: false,
+      desc: "not exists",
+    },
+  ];
 
-    expect(vol.toJSON("/foo/bar/test/foobar.test")).toMatchInlineSnapshot(`
+  data.forEach(async ({ type, path, expected, desc }) => t.test(
+    `return $expected if ${type} '${path}' ${desc}`,
+    async () => {
+      t.resolveMatch(fileExists(path), expected);
+    }
+  ));
+
+  t.end();
+});
+
+t.test("ensureDir()", async () => {
+  const data: {path: string, exists: boolean, desc: string}[]  = [
+    { path: "/testFolder", exists: true, desc: "not created if present" },
+    {
+      path: "/testNewFolder",
+      exists: false,
+      desc: "created if not present",
+    },
+  ]
+
+  data.forEach(async ({ path, exists, desc }) => t.test(`folder is ${desc}`, async () => {
+    t.resolveMatch(fileExists(path), exists);
+
+    await ensureDir(path);
+
+    t.resolveMatch(fileExists(path), true);
+  }));
+
+  t.end();
+});
+
+t.test("saveFile()", async () => {
+  t.test("create file and folders", async () => {
+    await saveFile("/foo/bar/test/foobar.test", "foobar file for t.test");
+
+    t.same(vol.toJSON("/foo/bar/test/foobar.test"),
         {
-          "/foo/bar/test/foobar.test": "foobar file for test",
+          "/foo/bar/test/foobar.test": "foobar file for t.test",
         }
-      `);
+      );
   });
 });
