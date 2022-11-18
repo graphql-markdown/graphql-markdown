39fda287f666db286b4ef3aed6cebc431568a8fc
Grégory Heitz <edno@edno.io>
2022-11-18 15:13:59 +0000

    remove index export from utils

--- packages/utils/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * as fs from "./fs";
-export * as prettier from "./prettier";
-export * as array from "./array";
-// export * as object from "./object";
-export * as string from "./string";
-export * as url from "./url";
-export * as graphql from "./graphql";
