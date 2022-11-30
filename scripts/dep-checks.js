f1381ace1f9f89557d8121ee4b89e867a47a0503
Grégory Heitz <edno@edno.io>
2022-11-30 09:27:12 +0000

    :recycle:  reduce cognitive complexity

--- scripts/dep-checks.js
+++ scripts/dep-checks.js
@@ -6,80 +6,76 @@ const rootDir = "..";
 
 const { dependencies: packages } = require(`${rootDir}/package.json`);
 
-(() => {
-  const packagesMap = (() => {
-    const map = {};
-    Object.entries(packages).forEach(([packageName, packagePath]) => {
-      const packageJson =
-        packagePath.replace("file:", `${rootDir}/`) + "/package.json";
-      const { version, dependencies, peerDependencies } = require(packageJson);
+const getWorkspacePackagesMap = () => {
+  const map = {};
+  Object.entries(packages).forEach(([packageName, packagePath]) => {
+    const packageJson =
+      packagePath.replace("file:", `${rootDir}/`) + "/package.json";
+    const { version, dependencies, peerDependencies } = require(packageJson);
 
-      Object.assign(map, {
-        [packageName]: { version, dependencies, peerDependencies },
-      });
+    Object.assign(map, {
+      [packageName]: { version, dependencies, peerDependencies },
     });
+  });
+  return map;
+};
 
-    return map;
-  })();
+const packagesMap = getWorkspacePackagesMap();
 
-  const checkPackage = (packageName, dependencies) => {
-    const report = [];
-    if (typeof dependencies === "undefined") {
+const checkPackage = (packageName, dependencies) => {
+  const report = [];
+  if (typeof dependencies === "undefined") {
+    return [];
+  }
+
+  Object.entries(dependencies).map(([depName, version]) => {
+    if (!depName.startsWith("@graphql-markdown")) {
       return [];
     }
 
-    Object.entries(dependencies).map(([depName, version]) => {
-      if (!depName.startsWith("@graphql-markdown")) {
-        return [];
-      }
+    if (typeof packagesMap[depName] === "undefined") {
+      report.push(
+        `${packageName} declares unknown workspace package ${depName}`,
+      );
+    } else if (
+      version.localeCompare(`^${packagesMap[depName].version}`) !== 0
+    ) {
+      report.push(
+        `${packageName} uses ${depName} "${version}" instead of "^${packagesMap[depName].version}"`,
+      );
+    }
+  });
 
-      if (typeof packagesMap[depName] === "undefined") {
-        report.push(
-          `${packageName} declares unknown workspace package ${depName}`,
-        );
-      } else if (
-        version.localeCompare(`^${packagesMap[depName].version}`) !== 0
-      ) {
-        report.push(
-          `${packageName} uses ${depName} "${version}" instead of "^${packagesMap[depName].version}"`,
-        );
-      }
-    });
+  return report;
+};
 
-    return report;
-  };
-
-  const reportDep = Object.entries(packagesMap)
-    .map(([packageName, { dependencies }]) => {
-      return checkPackage(packageName, dependencies);
+const generateReport = (packagesMap, type) => {
+  return Object.entries(packagesMap)
+    .map(([packageName, packageInfo]) => {
+      if (typeof packageInfo[type] === "undefined") return [];
+      return checkPackage(packageName, packageInfo[type]);
     })
     .flat();
+};
 
-  if (reportDep.length > 0) {
-    console.error(
-      `${reportDep.length} workspace dependencies error${
-        reportDep.length > 1 ? "s" : ""
-      }:`,
-    );
-    reportDep.forEach((log) => console.warn(`. ${log}`));
-  }
+(() => {
+  let errorCount = 0;
 
-  const reportPeerDep = Object.entries(packagesMap)
-    .map(([packageName, { peerDependencies }]) => {
-      return checkPackage(packageName, peerDependencies);
-    })
-    .flat();
+  const types = ["dependencies", "peerDependencies"];
+  types.forEach((type) => {
+    const report = generateReport(packagesMap, type);
+    errorCount += report.length;
+    if (report.length > 0) {
+      console.error(
+        `${report.length} workspace ${type} error${
+          report.length > 1 ? "s" : ""
+        }:`,
+      );
+      report.forEach((log) => console.warn(`. ${log}`));
+    }
+  });
 
-  if (reportPeerDep.length > 0) {
-    console.error(
-      `${reportPeerDep.length} workspace peerDependencies error${
-        reportPeerDep.length > 1 ? "s" : ""
-      }:`,
-    );
-    reportPeerDep.forEach((log) => console.warn(`. ${log}`));
-  }
-
-  const code = reportDep.length + reportPeerDep.length ? 1 : 0;
+  const code = errorCount ? 1 : 0;
   if (code === 0) {
     console.log("No error reported.");
   }
