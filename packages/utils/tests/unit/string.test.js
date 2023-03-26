9b2bec536aa4cb3efec2bc42e8db70e78ed97d05
Grégory Heitz <edno@edno.io>
2023-03-26 15:30:14 +0200

    :recycle:  string utils functions always return string

--- packages/utils/tests/unit/string.test.js
+++ packages/utils/tests/unit/string.test.js
@@ -4,55 +4,91 @@ const {
   stringCaseBuilder,
   prune,
   capitalize,
+  replaceDiacritics,
 } = require("../../src/string");
 
 describe("string", () => {
   describe("stringCaseBuilder()", () => {
     test("applies no transformation if one is not given", () => {
+      expect.hasAssertions();
+
       const text = "This is Not Transformed to lowercase.";
       const expected = "This is Not Transformed to lowercase";
+
       expect(stringCaseBuilder(text, undefined, " ")).toBe(expected);
     });
   });
 
+  describe("replaceDiacritics()", () => {
+    test("returns string without diacritic characters", () => {
+      expect.hasAssertions();
+
+      const text = "Âéêś";
+      const expected = "Aees";
+
+      expect(replaceDiacritics(text)).toBe(expected);
+    });
+
+    test("returns empty string is parameter is not a string", () => {
+      expect.hasAssertions();
+
+      const text = {};
+      const expected = "";
+
+      expect(replaceDiacritics(text)).toBe(expected);
+    });
+  });
+
   describe("prune()", () => {
-    test("Removes dash from beginning and/or end", () => {
-      let text = "-string-that-begins-and-ends-with-dash-";
-      let expected = "string-that-begins-and-ends-with-dash";
-      expect(prune(text, "-")).toBe(expected);
+    test.each([
+      {
+        text: "-string-that-begins-and-ends-with-dash-",
+        expected: "string-that-begins-and-ends-with-dash",
+      },
+      {
+        text: "-string-that-begins-with-dash",
+        expected: "string-that-begins-with-dash",
+      },
+      {
+        text: "string-that-ends-with-dash-",
+        expected: "string-that-ends-with-dash",
+      },
+      {
+        text: "string-with-no-dashes-in-beginning-or-end",
+        expected: "string-with-no-dashes-in-beginning-or-end",
+      },
+    ])("Removes dash from beginning and/or end", ({ text, expected }) => {
+      expect.hasAssertions();
 
-      text = "-string-that-begins-with-dash";
-      expected = "string-that-begins-with-dash";
-      expect(prune(text, "-")).toBe(expected);
-
-      text = "string-that-ends-with-dash-";
-      expected = "string-that-ends-with-dash";
-      expect(prune(text, "-")).toBe(expected);
-
-      text = "string-with-no-dashes-in-beginning-or-end";
-      expected = "string-with-no-dashes-in-beginning-or-end";
       expect(prune(text, "-")).toBe(expected);
     });
   });
 
   describe("toSlug()", () => {
     test("returns kebab style slug", () => {
+      expect.hasAssertions();
+
       const text = "This is not a slug, but you can use toSlug() function.";
       const expected = "this-is-not-a-slug-but-you-can-use-to-slug-function";
+
       expect(toSlug(text)).toBe(expected);
     });
   });
 
   describe("escapeMDX()", () => {
     test("returns string with HTML &#x0000; format for MDX special characters", () => {
+      expect.hasAssertions();
+
       expect(escapeMDX("{MDX} <special> characters")).toBe(
         "&#x007B;MDX&#x007D; &#x003C;special&#x003E; characters",
       );
     });
 
-    test("leaves input the same if it's not a string", () => {
-      expect(escapeMDX(5)).toBe(5);
-      expect(escapeMDX({ five: 5 })).toEqual({ five: 5 });
+    test("return a stringified version if not a string", () => {
+      expect.hasAssertions();
+
+      expect(escapeMDX(5)).toBe("5");
+      expect(escapeMDX({ five: 5 })).toBe("[object Object]");
     });
   });
 
@@ -69,6 +105,8 @@ describe("string", () => {
     ])(
       "returns capitalized string: first letter uppercase, rest lowercase",
       (input, expected) => {
+        expect.hasAssertions();
+
         expect(capitalize(input)).toBe(expected);
       },
     );
