0242dba3f3018dc5e7bb840e95f2229de14ccccf
Grégory Heitz <edno@edno.io>
2023-03-26 15:37:56 +0200

    :recycle:  add type guards on printDescription and printDeprecation

--- packages/printer-legacy/src/common.js
+++ packages/printer-legacy/src/common.js
@@ -6,20 +6,28 @@ const {
 const { MARKDOWN_EOP, NO_DESCRIPTION_TEXT } = require("./const/strings");
 
 const printDeprecation = (type) => {
-  if (!type.isDeprecated) {
+  if (
+    hasProperty(type, "isDeprecated") === false ||
+    type.isDeprecated === false
+  ) {
     return "";
   }
 
-  const reason = type.deprecationReason
-    ? ": " + escapeMDX(type.deprecationReason)
-    : "";
+  const reason =
+    hasProperty(type, "deprecationReason") &&
+    typeof type.deprecationReason === "string"
+      ? ": " + escapeMDX(type.deprecationReason)
+      : "";
   return `<Badge class="warning" text="DEPRECATED${reason}"/>${MARKDOWN_EOP}`;
 };
 
 const printDescription = (type, noText = NO_DESCRIPTION_TEXT) => {
   const description =
-    hasProperty(type, "description") && escapeMDX(type.description);
-  return `${printDeprecation(type)}${description || noText}`;
+    hasProperty(type, "description") && typeof type.description === "string"
+      ? escapeMDX(type.description)
+      : noText;
+  const deprecation = printDeprecation(type);
+  return `${deprecation}${description}`;
 };
 
 module.exports = {
