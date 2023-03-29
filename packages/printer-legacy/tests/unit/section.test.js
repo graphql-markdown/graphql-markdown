f87a01dac359c7c56fc924297ff151c8b695ac2b
Grégory Heitz <edno@edno.io>
2023-03-29 19:56:33 +0000

    :test_tube:  update test with empty line

--- packages/printer-legacy/tests/unit/section.test.js
+++ packages/printer-legacy/tests/unit/section.test.js
@@ -129,9 +129,11 @@ describe("section", () => {
         description: `Lorem ipsum dolor sit amet, 
 consectetur adipiscing elit, 
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
 Ut enim ad minim veniam, 
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
 Excepteur sint occaecat cupidatat non proident, 
 sunt in culpa qui officia deserunt mollit anim id est laborum.`,
       });
@@ -143,9 +145,11 @@ sunt in culpa qui officia deserunt mollit anim id est laborum.`,
         > Lorem ipsum dolor sit amet, 
         > consectetur adipiscing elit, 
         > sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        > 
         > Ut enim ad minim veniam, 
         > quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        > Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        > Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        > 
         > Excepteur sint occaecat cupidatat non proident, 
         > sunt in culpa qui officia deserunt mollit anim id est laborum.
         > "
