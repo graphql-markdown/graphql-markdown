7bf7eea359e5232e18ce032bd4fbac46bb2a8ce3
Lu Jiang <89472381+ljiang-ti@users.noreply.github.com>
2023-05-07 07:38:40 -0700

    Fix indentation level in code section (#851)

--- packages/printer-legacy/tests/unit/graphql/interface.test.js
+++ packages/printer-legacy/tests/unit/graphql/interface.test.js
@@ -12,17 +12,25 @@ const {
 } = require("../../../src/graphql/interface");
 
 describe("interface", () => {
+  const type = new GraphQLInterfaceType({
+    name: "TestInterfaceName",
+    fields: {
+      one: { type: GraphQLString },
+      two: { type: GraphQLBoolean },
+      three: {
+        type: GraphQLString,
+        args: {
+          four: {
+            type: GraphQLString,
+          },
+        },
+      },
+    },
+  });
+
   describe("printInterfaceMetadata()", () => {
     test("returns interface metadata", () => {
       expect.hasAssertions();
-
-      const type = new GraphQLInterfaceType({
-        name: "TestInterfaceName",
-        fields: {
-          one: { type: GraphQLString },
-          two: { type: GraphQLBoolean },
-        },
-      });
 
       const metadata = printInterfaceMetadata(type, DEFAULT_OPTIONS);
 
@@ -37,6 +45,12 @@ describe("interface", () => {
         > 
         > 
 
+        #### [<code style={{ fontWeight: 'normal' }}>TestInterfaceName.<b>three</b></code>](#)<Bullet />[\`String\`](/scalars/string) <Badge class="secondary" text="scalar"/>
+        > 
+        > ##### [<code style={{ fontWeight: 'normal' }}>TestInterfaceName.three.<b>four</b></code>](#)<Bullet />[\`String\`](/scalars/string) <Badge class="secondary" text="scalar"/>
+        > 
+        > 
+
         "
       `);
     });
@@ -46,20 +60,15 @@ describe("interface", () => {
     test("returns an interface with its fields", () => {
       expect.hasAssertions();
 
-      const type = new GraphQLInterfaceType({
-        name: "TestInterfaceName",
-        fields: {
-          one: { type: GraphQLString },
-          two: { type: GraphQLBoolean },
-        },
-      });
-
       const code = printCodeInterface(type);
 
       expect(code).toMatchInlineSnapshot(`
         "interface TestInterfaceName {
           one: String
           two: Boolean
+          three(
+            four: String
+          ): String
         }"
       `);
     });
