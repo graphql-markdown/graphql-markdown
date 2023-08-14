bc6de0a49a355b5ced403617958fc7ceb2be3668
Greg Heitz <edno@edno.io>
2023-08-14 13:25:22 +0200

    :wrench:  remove unused file in config

--- config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  jest: require("./jest.config"),
-  stryker: require("./stryker.conf"),
-  nyc: require("./nyc.config"),
-  eslint: require("./.eslintrc"),
-  prettier: require("./.prettierrc"),
-};
