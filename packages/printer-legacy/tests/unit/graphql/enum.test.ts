59b8403ab2b4f4119fdac7fd42647294c91f41f8
Greg Heitz <edno@edno.io>
2023-08-20 00:42:17 +0200

    :test_tube:  restore deleted tests from printer-legacy

---
+++ packages/printer-legacy/tests/unit/graphql/enum.test.ts
@@ -0,0 +1,124 @@
+import { GraphQLEnumType, GraphQLScalarType } from "graphql";
+
+import { printCodeEnum, printEnumMetadata } from "../../../src/graphql/enum";
+
+import { DEFAULT_OPTIONS } from "../../../src/const/options";
+
+describe("enum", () => {
+  const type = new GraphQLEnumType({
+    name: "EnumTypeName",
+    values: {
+      one: { value: "one" },
+      two: { value: "two", deprecationReason: "Deprecated" },
+    },
+  });
+
+  describe("printEnumMetadata()", () => {
+    test("returns enum metadata", () => {
+      expect.hasAssertions();
+
+      const metadata = printEnumMetadata(type, DEFAULT_OPTIONS);
+
+      expect(metadata).toMatchInlineSnapshot(`
+        "### Values
+
+        #### [<code style={{ fontWeight: 'normal' }}>EnumTypeName.<b>one</b></code>](#)  
+        > 
+        > 
+        > 
+        > 
+
+        #### [<code style={{ fontWeight: 'normal' }}>EnumTypeName.<b>two</b></code>](#) <Badge class="badge badge--deprecated badge--secondary" text="deprecated"/> 
+        > 
+        > 
+        > :::caution DEPRECATED
+        > Deprecated
+        > :::
+        > 
+        > 
+        > 
+
+        "
+      `);
+    });
+
+    test("returns enum metadata with grouped deprecated", () => {
+      expect.hasAssertions();
+
+      const metadata = printEnumMetadata(type, {
+        ...DEFAULT_OPTIONS,
+        deprecated: "group",
+      });
+
+      expect(metadata).toMatchInlineSnapshot(`
+        "### Values
+
+        #### [<code style={{ fontWeight: 'normal' }}>EnumTypeName.<b>one</b></code>](#)  
+        > 
+        > 
+        > 
+        > 
+
+         
+
+        <Details dataOpen={<><span className="deprecated">Hide deprecated</span></>} dataClose={<><span className="deprecated">Show deprecated</span></>}>
+
+        #### [<code style={{ fontWeight: 'normal' }}>EnumTypeName.<b>two</b></code>](#) <Badge class="badge badge--deprecated badge--secondary" text="deprecated"/> 
+        > 
+        > 
+        > :::caution DEPRECATED
+        > Deprecated
+        > :::
+        > 
+        > 
+        > 
+
+        </Details>
+
+        "
+      `);
+    });
+  });
+
+  describe("printCodeEnum()", () => {
+    test("returns enum code structure", () => {
+      expect.hasAssertions();
+
+      const code = printCodeEnum(type, DEFAULT_OPTIONS);
+
+      expect(code).toMatchInlineSnapshot(`
+        "enum EnumTypeName {
+          one
+          two @deprecated
+        }"
+      `);
+    });
+
+    test("returns enum code structure without deprecated if SKIP", () => {
+      expect.hasAssertions();
+
+      const code = printCodeEnum(type, {
+        ...DEFAULT_OPTIONS,
+        deprecated: "skip",
+      });
+
+      expect(code).toMatchInlineSnapshot(`
+        "enum EnumTypeName {
+          one
+        }"
+      `);
+    });
+
+    test("returns empty string if not enum type", () => {
+      expect.hasAssertions();
+
+      const scalarType = new GraphQLScalarType<unknown>({
+        name: "ScalarTypeName",
+      });
+
+      const code = printCodeEnum(scalarType, DEFAULT_OPTIONS);
+
+      expect(code).toBe("");
+    });
+  });
+});
---
+++ packages/printer-legacy/tests/unit/graphql/operation.test.ts
@@ -0,0 +1,193 @@
+import type { GraphQLSchema } from "graphql";
+import { GraphQLID, GraphQLObjectType, GraphQLString } from "graphql";
+
+jest.mock("@graphql-markdown/graphql", () => {
+  return {
+    ...jest.requireActual("@graphql-markdown/graphql"),
+    isDeprecated: jest.fn((T: any) => "deprecationReason" in T), // eslint-disable-line @typescript-eslint/no-explicit-any
+  };
+});
+
+import { DEFAULT_OPTIONS } from "../../../src/const/options";
+
+import {
+  printOperationMetadata,
+  printCodeOperation,
+} from "../../../src/graphql/operation";
+
+describe("operation", () => {
+  describe("printOperationMetadata()", () => {
+    test("returns operation metadata", () => {
+      expect.hasAssertions();
+
+      const operation = {
+        name: "TestQuery",
+        type: GraphQLID,
+        args: [],
+      };
+
+      const metadata = printOperationMetadata(operation, {
+        ...DEFAULT_OPTIONS,
+        schema: {
+          getType: () =>
+            new GraphQLObjectType({
+              name: "Test",
+              fields: {},
+            }),
+        } as unknown as GraphQLSchema,
+      });
+
+      expect(metadata).toMatchInlineSnapshot(`
+        "### Type
+
+        #### [\`Test\`](/objects/test) <Badge class="badge badge--secondary" text="object"/> 
+        > 
+        > 
+        > 
+        > 
+
+        "
+      `);
+    });
+
+    test("returns operation metadata with arguments", () => {
+      expect.hasAssertions();
+
+      const operation = {
+        name: "TestQuery",
+        type: GraphQLID,
+        args: [
+          {
+            name: "ArgFooBar",
+            type: GraphQLString,
+          },
+        ],
+      };
+
+      const metadata = printOperationMetadata(operation, {
+        ...DEFAULT_OPTIONS,
+        schema: {
+          getType: () =>
+            new GraphQLObjectType({
+              name: "Test",
+              fields: {},
+            }),
+        } as unknown as GraphQLSchema,
+      });
+
+      expect(metadata).toMatchInlineSnapshot(`
+        "### Arguments
+
+        #### [<code style={{ fontWeight: 'normal' }}>TestQuery.<b>ArgFooBar</b></code>](#)<Bullet />[\`String\`](/scalars/string) <Badge class="badge badge--secondary" text="scalar"/> 
+        > 
+        > 
+        > 
+        > 
+
+        ### Type
+
+        #### [\`Test\`](/objects/test) <Badge class="badge badge--secondary" text="object"/> 
+        > 
+        > 
+        > 
+        > 
+
+        "
+      `);
+    });
+
+    test("returns operation metadata with arguments with grouped deprecated", () => {
+      expect.hasAssertions();
+
+      const operation = {
+        name: "TestQuery",
+        type: GraphQLID,
+        args: [
+          {
+            name: "Foo",
+            type: GraphQLString,
+          },
+          {
+            name: "Bar",
+            type: GraphQLString,
+            deprecationReason: "Deprecated",
+          },
+        ],
+      };
+
+      const metadata = printOperationMetadata(operation, {
+        ...DEFAULT_OPTIONS,
+        deprecated: "group",
+        schema: {
+          getType: () =>
+            new GraphQLObjectType({
+              name: "Test",
+              fields: {},
+            }),
+        } as unknown as GraphQLSchema,
+      });
+
+      expect(metadata).toMatchInlineSnapshot(`
+        "### Arguments
+
+        #### [<code style={{ fontWeight: 'normal' }}>TestQuery.<b>Foo</b></code>](#)<Bullet />[\`String\`](/scalars/string) <Badge class="badge badge--secondary" text="scalar"/> 
+        > 
+        > 
+        > 
+        > 
+
+         
+
+        <Details dataOpen={<><span className="deprecated">Hide deprecated</span></>} dataClose={<><span className="deprecated">Show deprecated</span></>}>
+
+        #### [<code style={{ fontWeight: 'normal' }}>TestQuery.<b>Bar</b></code>](#)<Bullet />[\`String\`](/scalars/string) <Badge class="badge badge--deprecated badge--secondary" text="deprecated"/> <Badge class="badge badge--secondary" text="scalar"/> 
+        > 
+        > 
+        > :::caution DEPRECATED
+        > Deprecated
+        > :::
+        > 
+        > 
+        > 
+
+        </Details>
+
+        ### Type
+
+        #### [\`Test\`](/objects/test) <Badge class="badge badge--secondary" text="object"/> 
+        > 
+        > 
+        > 
+        > 
+
+        "
+      `);
+    });
+  });
+
+  describe("printCodeOperation()", () => {
+    test("returns an operation with its fields", () => {
+      expect.hasAssertions();
+
+      const operation = {
+        name: "TestQuery",
+        type: GraphQLID,
+        args: [
+          {
+            name: "ArgFooBar",
+            type: GraphQLString,
+          },
+        ],
+      };
+
+      const code = printCodeOperation(operation);
+
+      expect(code).toMatchInlineSnapshot(`
+        "TestQuery(
+          ArgFooBar: String
+        ): ID
+        "
+      `);
+    });
+  });
+});
