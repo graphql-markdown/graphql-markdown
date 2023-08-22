e76142dfc120cb6805939fa80d4c7d14ef360385
Greg Heitz <edno@edno.io>
2023-08-22 11:21:49 +0200

    :technologist:  refactor api docs scripts

--- scripts/docs-api.sh
+++ scripts/docs-api.sh
@@ -4,7 +4,7 @@ API_DIR=./api
 
 npm run typedoc -- --out ${API_DIR}
 
-find ${API_DIR}/*/ -type f -name "index.md" -delete
+find ${API_DIR}/*/ -type f \( -name "index.md" -or -name "index" \) -delete
 
 for d in $(find ${API_DIR}/*/ -mindepth 1 -type d); do 
   echo "{ \"label\": \"@graphql-markdown/$(basename ${d})\", \"link\": { \"type\": \"generated-index\" } }" > ${d}/_category_.json;
