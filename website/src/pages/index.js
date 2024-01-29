23f45d7d52e1664dfd0e78da821b6fc6e64db058
Grégory Heitz <324670+edno@users.noreply.github.com>
2024-01-29 13:23:07 +0100

    :memo:  add missing alt prop for website main page images (#1246)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -14,6 +14,7 @@ function HomepageHeader() {
         <img
           className={clsx("hero", styles.heroImage)}
           src="/img/preview.svg"
+          alt=""
         />
         <p className="hero__subtitle">
           <span style={{ color: "#36c46f" }}>Flexible</span> documentation for{" "}
@@ -69,7 +70,7 @@ function Thanks() {
       <div className="text--center">
         <h3 className="padding-bottom--md">Thanks to our contributors</h3>
         <a href="https://github.com/graphql-markdown/graphql-markdown/graphs/contributors">
-          <img src="https://contrib.rocks/image?repo=graphql-markdown/graphql-markdown&columns=8" />
+          <img src="https://contrib.rocks/image?repo=graphql-markdown/graphql-markdown&columns=8" alt="contributors" />
         </a>
       </div>
     </div>
