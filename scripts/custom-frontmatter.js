bfd77786d491f30fdd675acc4a442e0d3900e6af
Grégory Heitz <324670+edno@users.noreply.github.com>
2024-06-23 11:01:47 +0000

    :memo:  fix typedoc custom frontmatter script

--- scripts/custom-frontmatter.js
+++ scripts/custom-frontmatter.js
@@ -1,18 +1,18 @@
 const path = require("node:path/posix");
 const { ReflectionKind } = require("typedoc");
-const { FrontmatterEvent } = require("typedoc-plugin-frontmatter");
+const { MarkdownPageEvent } = require("typedoc-plugin-markdown");
 
 // All plugins should export a `load()` function
 module.exports = {
   load: (app) => {
     // Listen to PREPARE_FRONTMATTER event
-    app.renderer.on(FrontmatterEvent.PREPARE_FRONTMATTER, (event) => {
+    app.renderer.on(MarkdownPageEvent.BEGIN, (event) => {
       // Update event.frontmatter object using information from the page model as JSON
       event.frontmatter = {
         // add a title
-        title: path.basename(event.page.model?.name),
+        title: path.basename(event.model?.name),
         // add sidebar position for root project page
-        ...(event.page.model?.kindOf(ReflectionKind.Project) && {
+        ...(event.model?.kindOf(ReflectionKind.Project) && {
           sidebar_position: 1,
         }),
         // spread the existing frontmatter
