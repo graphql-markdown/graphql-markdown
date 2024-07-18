920c4c0e6652a0f7d8cb45ff3f8be99a9889a2a8
Grégory Heitz <324670+edno@users.noreply.github.com>
2024-07-18 14:32:39 +0000

    :technologist:  update husky setup

---
+++ .husky/_/husky.sh
@@ -0,0 +1,1 @@
+echo "husky - `#!/usr/bin/env sh` and `. "$(dirname -- "$0")/_/husky.sh"` lines in hooks are DEPRECATED and won't be supported in v10. You can remove these two lines for even simpler scripts"
