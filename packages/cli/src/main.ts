8be895759879fe12117a84beafea2981aecc098c
Greg Heitz <324670+edno@users.noreply.github.com>
2025-02-23 13:06:16 +0100

    :package:  downgrade commander to 5 for compatibility with docusaurus

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -8,11 +8,11 @@ import type {
 import { Command } from "commander";
 import { loadConfig } from "graphql-config";
 
-import GraphQLMarkdownCLI from "./cli";
+import { getGraphQLMarkdownCli } from "./cli";
 
 export const EXTENSION_NAME = "graphql-markdown" as const;
 export const graphQLConfigExtension: GraphQLExtensionDeclaration = () => {
-  return { name: EXTENSION_NAME } as const;
+  return { name: EXTENSION_NAME } as GraphQLExtensionDeclaration;
 };
 
 void (async (): Promise<void> => {
@@ -29,13 +29,11 @@ void (async (): Promise<void> => {
     : ["default"];
   // build commands for each project declared in graphql-config file
   for (const project of projects) {
-    const cmd = GraphQLMarkdownCLI({
+    const cmd = getGraphQLMarkdownCli({
       id: project,
     } as unknown as ConfigOptions);
     program.addCommand(cmd);
   }
-
-  program.showHelpAfterError(true);
 
   await program.parseAsync(process.argv);
 })();
