0d32ffd0f5fff519c78eb5ab12b4f44a972c66e3
Gregory Heitz <324670+edno@users.noreply.github.com>
2025-08-27 18:40:42 +0200

    :bug:  fix issue with object parameter with default value

--- packages/printer-legacy/tests/unit/graphql/operation.test.ts
+++ packages/printer-legacy/tests/unit/graphql/operation.test.ts
@@ -218,7 +218,7 @@ undefined### Type
 
     expect(code).toMatchInlineSnapshot(`
         "TestQuery(
-          ArgFooBar: TestInput = { foo: \\"bar\\" }
+          ArgFooBar: TestInput = { foo: "bar" }
         ): ID
         "
       `);
