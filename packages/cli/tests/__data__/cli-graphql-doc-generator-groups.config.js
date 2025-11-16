464270243fec3c51e67a62bd305feac985c87f09
Gregory Heitz <324670+edno@users.noreply.github.com>
2025-11-16 19:01:34 +0100

    fix: disable categorySortPrefix in test configs - incompatible with Docusaurus sidebar routing

--- packages/cli/tests/__data__/cli-graphql-doc-generator-groups.config.js
+++ packages/cli/tests/__data__/cli-graphql-doc-generator-groups.config.js
@@ -31,7 +31,7 @@ module.exports = {
         docOptions: {
           index: true,
           categorySort: "natural",
-          categorySortPrefix: true,
+          categorySortPrefix: false,
         },
         customDirective: {
           beta: {
--- packages/docusaurus/tests/__data__/docusaurus2-graphql-doc-nobuild.js
+++ packages/docusaurus/tests/__data__/docusaurus2-graphql-doc-nobuild.js
@@ -49,7 +49,7 @@ module.exports = {
         runOnBuild: false,
         docOptions: {
           categorySort: "natural",
-          categorySortPrefix: true,
+          categorySortPrefix: false,
         },
       },
     ],
